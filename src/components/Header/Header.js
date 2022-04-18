942cd0c34c2fa072c0aeff5884d8d99f3d690af4
mirajul alam <alifmirajulalam@gmail.comm>
2022-04-18 13:13:00 +0600

    custom link added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import auth from '../../firebase.init';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import CustomLink from '../CustomLink/CustomLink';
 const Header = () => {
     const [user] = useAuthState(auth);
     const handleSignOut = () => {
@@ -20,13 +21,13 @@ const Header = () => {
                         <Nav className="me-auto">
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to="/home" className='text-white'>Home</Nav.Link>
-                            <Nav.Link as={Link} to="/blogs" className='text-white'>Blogs</Nav.Link>
-                            <Nav.Link as={Link} to="/aboutus" className='text-white'>About Us</Nav.Link>
-                            {user ? <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>Log out</button> : <Nav.Link as={Link} to="/login" className='text-white'>LogIn</Nav.Link>}
-                            <Nav.Link as={Link} to="/signup" className='text-white'>
+                            <CustomLink as={Link} to="/home" className='text-white me-3'>Home</CustomLink>
+                            <CustomLink as={Link} to="/blogs" className='text-white me-3'>Blogs</CustomLink>
+                            <CustomLink as={Link} to="/aboutus" className='text-white me-3'>About Us</CustomLink>
+                            {user ? <button className='btn btn-link text-white text-decoration-none me-3' onClick={handleSignOut}>Log out</button> : <CustomLink as={Link} to="/login" className='text-white me-3'>LogIn</CustomLink>}
+                            <CustomLink as={Link} to="/signup" className='text-white me-3'>
                                 SignUp
-                            </Nav.Link>
+                            </CustomLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
